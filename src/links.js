9c0ff6b57a68d5049e7eebc67ac932e4ee003d7e
Vili Heikkilä <vili.heikkila@solita.fi>
2019-04-05 09:33:07 +0300

    removed some logging

--- src/links.js
+++ src/links.js
@@ -5,7 +5,6 @@ const dynamoDb = new aws.DynamoDB.DocumentClient();
 
 
 module.exports.put_link = (event, context, callback) => {
-  console.log("starting shit")
   const params = {
     TableName: "Link-Table",
     Item: {
@@ -14,34 +13,30 @@ module.exports.put_link = (event, context, callback) => {
     }
   }
 
-  console.log("just before put")
   dynamoDb.put(params, (error, data) => {
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
     };
 
-    console.log("before error check: ")
-    console.log(error)
     if (error) {
+      console.log(error)
       const res = {
         statusCode: 500,
         headers: headers,
-        body: JSON.stringify({ status: false })
+        body: JSON.stringify({
+          status: false
+        })
       };
-    console.log("before error callback")
-    callback(null, res);
-    console.log("after error callback")
-    return;
+      callback(null, res);
+      return;
     }
 
-    console.log("did not fail")
     const res = {
       statusCode: 200,
       headers: headers,
       body: JSON.stringify(params.Item)
     }
-    console.log("before final callback")
     callback(null, res);
   })
 };
