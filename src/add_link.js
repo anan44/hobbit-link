7b5e05ba987b9a22864a5bf8e2950e026827c9c1
Vili Heikkilä <vili.heikkila@solita.fi>
2019-04-14 17:45:54 +0300

    added ttl and check for alias conflict

--- src/add_link.js
+++ src/add_link.js
@@ -3,39 +3,61 @@ const aws = require('aws-sdk');
 const dynamoDb = new aws.DynamoDB.DocumentClient();
 
 module.exports.add_link = (event, context, callback) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true
+  };
   const body = JSON.parse(event.body)
+  const deleteTime = Math.floor(new Date(body.deleteDate).getTime() / 1000)
   const params = {
     TableName: "LinkTable",
     Item: {
       alias: body.alias,
-      real: body.real
+      real: body.real,
+      timeToLive: deleteTime
+    }
+  }
+  const checkParams = {
+    TableName: "LinkTable",
+    Key: {
+      alias: body.alias
     }
   }
 
-  dynamoDb.put(params, (error, data) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    if (error) {
-      console.log(error)
+  dynamoDb.get(checkParams, (error, data) => {
+    console.log(data)
+    if (data && data.Item) {
+      console.log("Alias conflict!")
       const res = {
-        statusCode: 500,
+        statusCode: 403,
         headers: headers,
         body: JSON.stringify({
-          status: false
+          status: false,
+          error: "Alias conflict. Requested alias is currently taken. Try another alias."
         })
-      };
+      }
       callback(null, res);
-      return;
+      return
     }
-
-    const res = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    }
-    callback(null, res);
+    dynamoDb.put(params, (error, data) => {
+      if (error) {
+        console.log(error)
+        const res = {
+          statusCode: 500,
+          headers: headers,
+          body: JSON.stringify({
+            status: false
+          })
+        };
+        callback(null, res);
+        return;
+      }
+      const res = {
+        statusCode: 200,
+        headers: headers,
+        body: JSON.stringify(params.Item)
+      }
+      callback(null, res);
+    })
   })
 };
